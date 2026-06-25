191da8d443bfe107cda9b5c22023eb1ae6973de8
maxvasyt <maksu.vasu@mail.ru>
2026-06-25 19:44:39 +0300

    task 2 on maps is ready

--- map/task2/task2.go
+++ map/task2/task2.go
@@ -1,6 +1,10 @@
 package main
 
-import "strings"
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
 
 // WordFrequency принимает строку текста и возвращает map с частотой слов.
 func WordFrequency(text string) map[string]int {
@@ -17,14 +21,27 @@ func WordFrequency(text string) map[string]int {
 // PrintWordFrequency выводит частотный анализ слов, отсортированный по убыванию частоты.
 func PrintWordFrequency(freqMap map[string]int) {
 	// TODO: Реализуйте функцию.
-	sort_map := make(map[string]int)
-	for value := range freqMap {
-		sort_map[value] = freqMap[value]
+	keys := make([]string, 0, len(freqMap))
+	for word := range freqMap {
+		keys = append(keys, word)
+	}
+
+	//Внутри функции обрабатывается правило (func(i,j int) bool{}).
+	// Также внутри есть умение сортировать.
+	//
+	sort.Slice(keys, func(i, j int) bool {
+		return freqMap[keys[i]] > freqMap[keys[j]]
+	})
+
+	//Проходимся по отсортированному срезу и печатаем слова из карты
+	for _, word := range keys {
+		fmt.Printf("%s: %d\n", word, freqMap[word])
 	}
 }
 
 func main() {
-
-	text := "golang is great and golang is fast"
-
+	freqMap := make(map[string]int)
+	text := "golang is great and and and and golang is fast fast fast"
+	freqMap = WordFrequency(text)
+	PrintWordFrequency(freqMap)
 }
